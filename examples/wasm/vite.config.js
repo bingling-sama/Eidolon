d70e8af9fbcefb6e00433c12cf8360852eeecd94
bingling-sama <bingling_sama@qq.com>
2026-06-28 00:59:31 +0800

    ✨ feat: GitHub Actions workflow to deploy WASM example to Pages

--- examples/wasm/vite.config.js
+++ examples/wasm/vite.config.js
@@ -3,5 +3,6 @@ import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
+  base: process.env.BASE_URL || '/',
   plugins: [wasm(), topLevelAwait()],
 });
